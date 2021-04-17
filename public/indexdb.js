41fe87ada4c14a47f269f857e2086a9bb3e5ba67
jcerb <brt.jst@gmail.com>
2021-04-17 15:55:00 -0700

    investigate console error

--- public/indexdb.js
+++ public/indexdb.js
@@ -66,7 +66,7 @@ function checkDatabase() {
           if (res.length !== 0) {
             // Open another transaction to BudgetStore with the ability to read and write
             transaction = db.transaction(["pending"], "readwrite");
-
+            // console error on line 68: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Invalid_const_assignment
             // Assign the current store to a variable
             const currentStore = transaction.objectStore("pending");
 
