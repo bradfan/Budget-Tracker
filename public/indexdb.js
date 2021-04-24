827d78921b8c35ccae68631696d861e00ce08bf5
jcerb <brt.jst@gmail.com>
2021-04-23 19:22:11 -0700

    debug,cleanup

--- public/indexdb.js
+++ public/indexdb.js
@@ -1,95 +1,50 @@
-// bring in model/example from class repo (17-26)
-let db;
-// define indexedDB
 const indexedDB = window.indexedDB;
-// Create a new db request for a "budget" database.
+let db;
 const request = indexedDB.open("budget", 1);
-
 request.onupgradeneeded = ({ target }) => {
   let db = target.result;
-  db.createObjectStore("pending", { autoIncrenment: true });
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 request.onsuccess = ({ target }) => {
   db = target.result;
-  // app online prior to reading db
+  // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
-  //   for coming back online and retrieve pending from "const transaction = db.transaction(["pending"], "readwrite");""
 };
-request.onerror = function (event) {
-  console.log("Whoops!" + event.data.errorCode);
+request.onerror = function(event) {
+  console.log("Woops! " + event.target.errorCode);
 };
-
-// this is crucial to the next homework
-const saveRecord = (record) => {
-  console.log("Save record invoked");
-  // Create a transaction on the BudgetStore db with readwrite access
+function saveRecord(record) {
   const transaction = db.transaction(["pending"], "readwrite");
-
-  // Access your BudgetStore object store
   const store = transaction.objectStore("pending");
-
-  // Add record to your store with add method.
   store.add(record);
-};
-
+}
 function checkDatabase() {
-  console.log("check db invoked");
-
-  // Open a transaction on your BudgetStore db
   const transaction = db.transaction(["pending"], "readwrite");
-
-  // access your BudgetStore object
   const store = transaction.objectStore("pending");
-
-  // Get all records from store and set to a variable
   const getAll = store.getAll();
-
-  // If the request was successful
-  getAll.onsuccess = function () {
-    // If there are items in the store, we need to bulk add them when we are back online
+  getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
           Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json",
-        },
+          "Content-Type": "application/json"
+        }
       })
-        .then((response) => {
-          return response.json();
-        })
-        .then((res) => {
-          // If our returned response is not empty
-          if (res.length !== 0) {
-            // Open another transaction to BudgetStore with the ability to read and write
-            let transaction = db.transaction(["pending"], "readwrite");
-            // console error on line 68: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Errors/Invalid_const_assignment
-            // Assign the current store to a variable
-            const currentStore = transaction.objectStore("pending");
-
-            // Clear existing entries because our bulk add was successful
-            currentStore.clear();
-            console.log("Clearing store 🧹");
-          }
-        });
+      .then(response => {       
+        return response.json();
+      })
+      .then(() => {
+        // delete records if successful
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
     }
   };
 }
-
-// was in the original c/p - don't think it is neccesary
-// request.onsuccess = function (e) {
-//   console.log('success');
-//   db = e.target.result;
-
-//   // Check if app is online before reading from db
-//   if (navigator.onLine) {
-//     console.log('Backend online! 🗄️');
-//     checkDatabase();
-//   }
-// };
-
-// Listen for app coming back online
+// listen for app coming back online
 window.addEventListener("online", checkDatabase);
